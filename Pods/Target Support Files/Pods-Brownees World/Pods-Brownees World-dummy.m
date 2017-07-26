542ec5c8935d4091337960971fb9f9de179d390e
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-07-25 21:11:26 -0700

    .gitignore now working

--- Pods/Target Support Files/Pods-Brownees World/Pods-Brownees World-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Brownees_World : NSObject
-@end
-@implementation PodsDummy_Pods_Brownees_World
-@end
--- Pods/Target Support Files/Pods-Brownees World/Pods-Brownees World-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Brownees_WorldVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Brownees_WorldVersionString[];
-
